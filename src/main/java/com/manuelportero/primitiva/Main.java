d2d82eda4a29845a04bf3e663d87aff4f0f7fe0e
Nolete19 <manoloporteropons@gmail.com>
2023-02-18 14:57:24 +0100

    Prueba2
    Esto es solo una prueba

--- src/main/java/com/manuelportero/primitiva/Main.java
+++ src/main/java/com/manuelportero/primitiva/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola soy Luis");
+        System.out.println("Hola soy Manuel");
     }
 }
