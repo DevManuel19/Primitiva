fa99132857292c8ce139de8f961b4f28c70863ef
Nolete19 <manoloporteropons@gmail.com>
2023-02-18 15:05:30 +0100

    Prueba4

--- src/main/java/com/manuelportero/primitiva/Main.java
+++ src/main/java/com/manuelportero/primitiva/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hola soy Luis");
         System.out.println("Hola soy Manuel");
+        System.out.println("Suicidar");
     }
 }
