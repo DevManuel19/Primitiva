c5d79a9666af160ad7f648f0e6f36faa4dd6760b
Nolete19 <manoloporteropons@gmail.com>
2023-02-19 18:13:32 +0100

    Implementación de métodos
    Implementación de los primeros métodos de generación de números aleatorios

--- src/main/java/com/manuelportero/primitiva/RandomNumbers.java
+++ src/main/java/com/manuelportero/primitiva/RandomNumbers.java
@@ -1,4 +1,32 @@
 package com.manuelportero.primitiva;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RandomNumbers {
+    private static Random random = new Random();
+    private int[] bombo1;
+    private int[] bombo2;
+    private int max = 49;
+    private int min = 1;
+
+    public int[] rellenarBombo1(){
+        for(int i = 0;i < 50;i++){
+            bombo1[i] = i+1;
+        }
+        return bombo1;
+    }
+    public int[] rellenarBombo2(){
+        for(int i = 0;i < 10;i++){
+            bombo2[i] = i;
+        }
+        return bombo2;
+    }
+    public int[] generarSeis(int[] bombo1){
+        int[] boleto = new int[6];
+        for (int i = 0;i < boleto.length;i++){
+             boleto[i] = random.nextInt(max - min + min) + 1;
+        }
+        return boleto;
+    }
 }
