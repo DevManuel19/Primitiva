10c53d51fefcdb1f41334c7753472e7f41ba7f7f
Luwei3D <115004166+Luwei3D@users.noreply.github.com>
2023-02-19 19:15:10 +0100

    Update GameMode.java

--- src/main/java/com/manuelportero/primitiva/GameMode.java
+++ src/main/java/com/manuelportero/primitiva/GameMode.java
@@ -1,7 +1,7 @@
 package com.manuelportero.primitiva;
 
 public class GameMode {
-    public void juegoUnico(int[] num, int[] premio, int reintegro , int bonificacion ,int premioReintegro, int premioBonificacion{
+    public void juegoUnico(int[] num, int[] premio, int reintegro , int bonificacion ,int premioReintegro, int premioBonificacion){
         int premios = 0;
         boolean isReintegros = false;
         boolean isBonificaciones = false;
@@ -39,6 +39,58 @@ public class GameMode {
             System.out.println("quinto premio");
 
         }
+        //else MenuModalidad();
 
+    }
+    public void  UntilPrize(int[] num, int[] premio, int reintegro , int bonificacion ,int premioReintegro, int premioBonificacion){
+       boolean isPremio= false;
+        do{ int premios = 0;
+        boolean isReintegros = false;
+        boolean isBonificaciones = false;
+        for (int i = 0; i < 6; i++) {
+            if (num[i] ==premio[i]);
+            premios++;
+
+        }
+        if (premioReintegro == reintegro){
+            isReintegros = true;
+
+        }
+        else if (bonificacion == premioBonificacion){
+            isBonificaciones = true;
+        }
+        else if ((premios == 6 && isReintegros) == true){
+            System.out.println("Premio especial");
+            isPremio= true;
+        }
+        else if (premios == 6) {
+            System.out.println("primer premio");
+            isPremio= true;
+
+        }
+        else if (premios == 5 && isBonificaciones == true) {
+            System.out.println("segundo premio");
+            isPremio= true;
+
+        }
+        else if (premios == 5) {
+            System.out.println("tercer premio");
+            isPremio= true;
+
+        }
+        else if (premios == 4) {
+            System.out.println("cuarto premio");
+            isPremio= true;
+
+        }
+        else if (premios == 3) {
+            System.out.println("quinto premio");
+            isPremio= true;
+
+        }
+        //else Main();
+
+
+        }while (isPremio == false);
     }
 }
