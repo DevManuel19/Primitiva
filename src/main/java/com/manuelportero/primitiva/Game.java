6c3acc8cf999e942011b0b8fccfb547129c34847
Luwei3D <115004166+Luwei3D@users.noreply.github.com>
2023-02-26 17:22:17 +0100

    Added a prize checker
    put a series of else ifs that checks the prize that the user receives

--- src/main/java/com/manuelportero/primitiva/Game.java
+++ src/main/java/com/manuelportero/primitiva/Game.java
@@ -36,6 +36,7 @@ public class Game {
         int isReintegro = 0;
         int isComplementario = 0;
         int aciertos = 0;
+        String respuesta="";
 
         do {
             isComplementario=0;
@@ -61,7 +62,21 @@ public class Game {
             count++;
         } while (aciertos < 3 && isReintegro==0);
         int[] resultado = new int[]{aciertos, isComplementario, isReintegro};
-        String respuesta="";
+        if (aciertos == 6 && isReintegro==1){
+            respuesta="Premio Especial, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        } else if (aciertos==6) {
+            respuesta="primerPremio , tu boleto es : "+ boletoUsuario + " y el ganador es :  "+ boletoGanador + " n veces"+ count;
+        } else if (aciertos== 5 && isComplementario==1)  {
+            respuesta="Segundo premio, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        } else if (aciertos==5) {
+            respuesta="Tercer premio, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        } else if (aciertos==4) {
+            respuesta="Cuarto premio, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        } else if (aciertos==3) {
+            respuesta="Quinto premio , tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        } else if (isReintegro==1) {
+            respuesta="solo reintegro  tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        }
         return respuesta;
     }
     public String hastaPremioSinReintegro (String boletoUsuario) {
@@ -70,6 +85,7 @@ public class Game {
         int isReintegro = 0;
         int isComplementario = 0;
         int aciertos = 0;
+        String respuesta="";
         do {
             isReintegro=0;
             isComplementario = 0;
@@ -95,7 +111,21 @@ public class Game {
             count++;
         } while (aciertos < 3);
         int[] resultado = new int[]{aciertos, isComplementario, isReintegro};
-        String respuesta="";
+        if (aciertos == 6 && isReintegro==1){
+            respuesta="Premio Especial, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces"+ count;
+        } else if (aciertos==6) {
+            respuesta="primerPremio , tu boleto es : "+ boletoUsuario + " y el ganador es :  "+ boletoGanador+ " n veces "+ count ;
+        } else if (aciertos== 5 && isComplementario==1)  {
+            respuesta="Segundo premio, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador+ " n veces "+ count ;
+        } else if (aciertos==5) {
+            respuesta="Tercer premio, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces "+ count;
+        } else if (aciertos==4) {
+            respuesta="Cuarto premio, tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces " + count;
+        } else if (aciertos==3) {
+            respuesta="Quinto premio , tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces "+ count;
+        } else if (isReintegro==1) {
+            respuesta="solo reintegro  tu boleto es : "+ boletoUsuario+" y el ganador es : " + boletoGanador + " n veces "+ count;
+        }
         return respuesta;
     }
     public String hastaDiezMil (String boletoUsuario) {
@@ -190,98 +220,4 @@ public class Game {
          bombo.rellenarBombo2();
     }
 
-
-
-
-    /*public void juegoUnico(int[] num, int[] premio, int reintegro , int bonificacion ,int premioReintegro, int premioBonificacion){
-        int premios = 0;
-
-        for (int i = 0; i < 6; i++) {
-            if (num[i] ==premio[i]);
-            premios++;
-
-        }
-        if (premioReintegro == reintegro){
-            isReintegros = true;
-        }
-        else if (bonificacion == premioBonificacion){
-            isBonificaciones = true;
-        }
-        else if ((premios == 6 && isReintegros) == true){
-            System.out.println("Premio especial");
-        }
-        else if (premios == 6) {
-            System.out.println("primer premio");
-
-        }
-        else if (premios == 5 && isBonificaciones == true) {
-            System.out.println("segundo premio");
-
-        }
-        else if (premios == 5) {
-            System.out.println("tercer premio");
-
-        }
-        else if (premios == 4) {
-            System.out.println("cuarto premio");
-
-        }
-        else if (premios == 3) {
-            System.out.println("quinto premio");
-
-        }
-        //else MenuModalidad();
-
-    }
-    public void  UntilPrize(int[] num, int[] premio, int reintegro , int bonificacion ,int premioReintegro, int premioBonificacion){
-       boolean isPremio= false;
-        do{ int premios = 0;
-        boolean isReintegros = false;
-        boolean isBonificaciones = false;
-        for (int i = 0; i < 6; i++) {
-            if (num[i] ==premio[i]);
-            premios++;
-
-        }
-        if (premioReintegro == reintegro){
-            isReintegros = true;
-
-        }
-        else if (bonificacion == premioBonificacion){
-            isBonificaciones = true;
-        }
-        else if ((premios == 6 && isReintegros) == true){
-            System.out.println("Premio especial");
-            isPremio= true;
-        }
-        else if (premios == 6) {
-            System.out.println("primer premio");
-            isPremio= true;
-
-        }
-        else if (premios == 5 && isBonificaciones == true) {
-            System.out.println("segundo premio");
-            isPremio= true;
-
-        }
-        else if (premios == 5) {
-            System.out.println("tercer premio");
-            isPremio= true;
-
-        }
-        else if (premios == 4) {
-            System.out.println("cuarto premio");
-            isPremio= true;
-
-        }
-        else if (premios == 3) {
-            System.out.println("quinto premio");
-            isPremio= true;
-
-        }
-        //else Main();
-
-
-        }while (isPremio == false);
-    }*/
 }
